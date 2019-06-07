4279c2aa1cb7c9d95fe5057ce224f9d23e8fbb16
tsantek <santek.vg@gmail.com>
2019-06-07 09:55:13 -0700

    server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+    console.info('listening on %d', port);
+});
